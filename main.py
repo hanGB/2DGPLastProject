01130f5e1dfb155141ac2eea285bd9b87a159851
hanGB <sy0871@naver.com>
2019-10-13 20:05:32 +0900

    임시 문 제작, 방 8방향 회전

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ now_dir = os.path.join(basic_dir, "resource/map")
 os.chdir(now_dir)
 
 background_map = [load_image('colormap.png'), load_image('colormapdia.png')]
-
+door = [load_image('frontdoor.png'), load_image('leftdoor.png'), load_image('rightdoor.png'),
+        load_image('dialeftdoor.png'), load_image('diarightdoor.png')]
 now_dir = os.path.join(basic_dir, "resource/enemy")
 os.chdir(now_dir)
 enemy = load_image('testdemon.png')
@@ -35,6 +36,7 @@ def show_ui():
     ui2.clip_draw(500, 0, 230, 100, 454, 122)
     uiAct.clip_draw(300 * act, 0, 300, 100, 190, 58)
 
+
 def input_key_in_battle():
     global act
     global game
@@ -42,11 +44,12 @@ def input_key_in_battle():
     events = get_events()
 
     for event in events:
-       if event.type == SDL_QUIT:
-           game = False
-       elif event.type == SDL_KEYDOWN:
-           if event.key == SDLK_DOWN:
-              act = act % 3 + 1
+        if event.type == SDL_QUIT:
+            game = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_DOWN:
+                act = act % 3 + 1
+
 
 def input_key_in_map():
     global map
@@ -55,22 +58,29 @@ def input_key_in_map():
     events = get_events()
 
     for event in events:
-       if event.type == SDL_QUIT:
-           game = False
-       elif event.type == SDL_KEYDOWN:
-           if event.key == SDLK_q:
-              map = (map + 1) % 2
-           elif event.key == SDLK_e:
-               map = (map + 1) % 2
+        if event.type == SDL_QUIT:
+            game = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_q:
+                map = (map - 1) % 8
+            elif event.key == SDLK_e:
+                map = (map + 1) % 8
+
 
 act = 0
 game = True
 i = 0
-map = 0;
+map = 0
+door_location = [(0, 2), (3, 4), (1, 0), (-1, 3), (-1, 1), (-1, -1), (2, -1), (4, -1)]
 
 while game:
     clear_canvas()
-    background_map[map].draw(640, 360)
+    background_map[map % 2].draw(640, 360)
+    if door_location[map][0] != -1:
+        door[door_location[map][0]].draw(640, 360)
+    if door_location[map][1] != -1:
+        door[door_location[map][1]].draw(640, 360)
+
     # show_ui()
     input_key_in_map()
     # enemy.clip_draw(0 + 86 * i, 0, 85, 100, 640, 460)
